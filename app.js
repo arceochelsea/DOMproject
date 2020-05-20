7f83a646272be69f7c7161e27db362b9fad6c32c
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-05-20 18:16:51 -0400

    Tutorial 5 Nodes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let books = document.querySelectorAll('#book-list li .name');
+const banner = document.querySelector('#page-banner');
 
-Array.from(books).forEach(function(book){
-    book.textContent += ' (book title)'; // this is how you append something to a string
-})
+console.log('#page-banner node type is:', banner.nodeType); //will tell us what the node type (node type - return values) in this case an element
+console.log('#page-banner node name is:', banner.nodeName); //will tell us element name! it is a DIV (type of element)
+console.log('#page-banner has child nodes:', banner.hasChildNodes()); //returns true or false! does it have child nodes?
 
-const bookList = document.querySelector('#book-list');
-//bookList.innerHTML = '<h2>Books and more books.. </h2>';
-bookList.innerHTML += '<p>This is how you add HTML</p>';
+
+const clonedBanner = banner.cloneNode(true); //we want it to go deeply into node. will just give node otherwise but not the nested content! thats why we must put true!
+console.log(clonedBanner);
