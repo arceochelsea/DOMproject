ded22d5b2e55a18323779ccafd2bc9aa7dad728f
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-05-19 23:55:16 -0400

    Tutorial 4 Query Selector

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-let titles = document.getElementsByClassName('title');
+const wmf = document.querySelector('#book-list li:nth-child(2).name');
+//console.log(wmf);
 
-// console.log(Array.isArray(titles)); //will return false if not an array true if it is
-// console.log(Array.isArray(Array.from(titles))); //we want to make an array from titles collection which will then trigger the isArray method to be true 
+let books = document.querySelector('#book-list li .name'); //will only return one, usually the first one
+//console.log(books);
 
-Array.from(titles).forEach(function(item){ //array from titles turns it into array 
-    console.log(item);
+books = document.querySelectorAll('#book-list li .name'); //this will grab several 
+//console.log(books);
+
+Array.from(books).forEach(function(book){
+    console.log(book);
 })
