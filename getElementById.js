29b416f5d2645b992263c02168bc5b96fd7c404b
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-05-19 23:27:58 -0400

    Tutorial 2 Get Element By Id

---
+++ getElementById.js
@@ -0,0 +1,13 @@
+//In the DOM
+
+
+// document
+// #document<!doctype html><html>​<head>​…​</head>​<body>​…​</body>​</html>​
+// document.getElementById('page-banner')
+// <div id=​"page-banner">​<h1 class=​"title">​Bookorama​</h1>​<p>​Books for Ninjas​</p>​<form id=​"search-books">​…​</form>​</div>​
+// let banner = document.getElementById('page-banner')
+// undefined
+// let bookList = document.getElementById('book-list')
+// undefined
+// bookList
+// <div id=​"book-list">​…​</div>​
