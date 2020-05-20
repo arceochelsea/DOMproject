95c5b61a57b8d0237d6598a82ecab6695aa6f3c2
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-05-20 01:03:00 -0400

    Tutorial 5 Changing Text and HTML Content

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const wmf = document.querySelector('#book-list li:nth-child(2).name');
-//console.log(wmf);
-
-let books = document.querySelector('#book-list li .name'); //will only return one, usually the first one
-//console.log(books);
-
-books = document.querySelectorAll('#book-list li .name'); //this will grab several 
-//console.log(books);
+let books = document.querySelectorAll('#book-list li .name');
 
 Array.from(books).forEach(function(book){
-    console.log(book);
+    book.textContent += ' (book title)'; // this is how you append something to a string
 })
+
+const bookList = document.querySelector('#book-list');
+//bookList.innerHTML = '<h2>Books and more books.. </h2>';
+bookList.innerHTML += '<p>This is how you add HTML</p>';
