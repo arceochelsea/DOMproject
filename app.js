8cfddffa761ffb20664b1ab510d261c021877f14
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-05-20 23:41:00 -0400

    Tutorial 12 Creating Elements

--- app.js
+++ app.js
@@ -16,6 +16,20 @@ const addForm = document.forms['add-book'];
 addForm.addEventListener('submit', function(e){
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
-    console.log(value);
+
+    //create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    //add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    //append to DOM
+    li.appendChild(bookName); //this is how to we add to DOM, appending adds to the end
+    li.appendChild(deleteBtn); //order matters, if switched delete button will go first and thats not what we want!
+    list.appendChild(li);
+
 
 });
