72777c8fe1148847c34e2c6b4444f5ead32d033c
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-05-24 23:46:19 -0400

    Custom Search Filter

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ addForm.addEventListener('submit', function(e){
 
 });
 
+//hide books
+
 const hideBox = document.querySelector('#hide');
 hideBox.addEventListener('change', function(e){
     if(hideBox.checked){
@@ -45,4 +47,20 @@ hideBox.addEventListener('change', function(e){
     } else {
         list.style.display = 'initial';
     }
+});
+
+//filter books
+
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach(function(book){
+        const title = book.firstElementChild.textContent;
+        if(title.toLowerCase().indexOf(term) != -1){
+            book.style.display = 'block';
+        } else {
+            book.style.display = 'none';
+        }
+    })
 });
